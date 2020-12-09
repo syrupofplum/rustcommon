cf3dfa9df59df22dc3dba8ef5301d8d13945a809
syrupofplum <syrupofplum@gmail.com>
2020-12-09 16:38:55 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,17 @@ mod tests {
     }
 }
 
+#[cfg(any(feature="mysql_async", feature="mysql_pool_async"))]
 pub mod mysqlaccessor;
+#[cfg(feature="mysql_async")]
 pub mod mysqlaccessor_async;
+#[cfg(feature="mysql_pool_async")]
 pub mod mysqlaccessor_pool_async;
+#[cfg(feature="http_async")]
 pub mod httpaccessor;
+#[cfg(any(feature="redis_async", feature="redis_actix"))]
 pub mod redisaccessor;
+#[cfg(feature="redis_async")]
 pub mod redisaccessor_async;
+#[cfg(feature="redis_actix")]
 pub mod redisaccessor_actix;
